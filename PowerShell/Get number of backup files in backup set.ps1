d0556e266bd17f3a04a88deed82d04fd140977af
Matt Wilhoite <149613892+mewilhoite@users.noreply.github.com>
2024-11-08 08:35:10 -0500

    Update Get number of backup files in backup set.ps1

--- PowerShell/Get number of backup files in backup set.ps1
+++ PowerShell/Get number of backup files in backup set.ps1
@@ -1,4 +1,6 @@
 <# GET NUMBER OF BACKUP FILES IN THE BACKUP SET #>
+<# YOU CAN USE ANY FILE IN THE SET FOR THE URL #>
+$backupFileURLLabelCheck = "<full URL of blob backup>"
 $query = "RESTORE LabelOnly FROM URL = '$backupFileURLLabelCheck'"
 $labelQueryResult = invoke-sqlcmd -ServerInstance $targetForRestores -database master -Query $query -TrustServerCertificate
 $NumberOfBackupfiles =  $labelQueryResult.familyCount
